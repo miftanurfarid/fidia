8c0f3acee229622451f0833251802b6e2d2ea86d
miftanurfarid <miftanurfarid@gmail.com>
2018-06-08 13:20:04 +0700

    zero di bidang median

--- code/estTheta.m
+++ code/estTheta.m
@@ -5,5 +5,6 @@ function doa = estTheta(etd,c,d)
     % d   = microphone - microphone
 
     % doa = real(rad2deg(acos(etd * c / d))); % in deg
-    doa = real(acos(etd * c / d)); % in rad
+    % doa = real(acos(etd * c / d)); % in rad
+    doa = real(asin(etd * c / d));
 end
